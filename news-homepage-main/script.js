4c1d6fcb6249e2276a3178a81116085c8854b1ad
Cristiano Tati <tatic9525@gmail.com>
2026-05-26 08:12:53 +0100

    atulizei

---
+++ news-homepage-main/script.js
@@ -0,0 +1,7 @@
+
+    const hambuguer = document.getElementById('hambuguer')
+    const menu = document.getElementById('menu')
+
+    hambuguer.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
